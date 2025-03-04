3e60056fe93e40e7465773fab47e6974923d4da0
Dicedex-LLC <mwaangamwaanga0@gmail.com>
2025-03-04 13:33:45 +0200

    Update app_strings.dart

--- lib/assets/constants/app_strings.dart
+++ lib/assets/constants/app_strings.dart
@@ -1,1 +1,8 @@
 
+class AppStrings {
+  static const String appName = "Bloom";
+  static const String appTagline = "Find your perfect match";
+  static const String loginTitle = "Welcome Back!";
+  static const String signupTitle = "Create Account";
+  // Add more strings as needed for your app
+}
